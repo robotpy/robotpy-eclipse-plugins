32999c10aa707a869d822a8a5fca867677964c53
Dustin Spicuzza <dustin@virtualroadside.com>
2018-02-10 00:09:19 -0500

    Update deploy script to deploy a specific version

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,8 +2,8 @@
 
 cd "$(dirname $0)"/..
 
-if [ ! -f pushToBintray.sh ]; then 
+if [ ! -f pushToBintray.sh ]; then
     wget https://raw.githubusercontent.com/robotpy/bintray-publish-p2-updatesite/master/pushToBintray.sh
 fi
 
-bash pushToBintray.sh $DEPLOY_USERNAME $DEPLOY_SECRET robotpy eclipse-plugin eclipse-plugin latest robotpy.updatesite/target/site
+bash pushToBintray.sh $1 $2 robotpy eclipse-plugin eclipse-plugin $3 robotpy.updatesite/target/site
